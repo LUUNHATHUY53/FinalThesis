77dd7cc19e02d2eb1eb129db5d3e71100e08c78a
dangphatthinh <101922346+dangphatthinh@users.noreply.github.com>
2022-06-07 16:32:44 +0700

    Update MPU6050_test_YMFC.ino

--- MPU6050_test_YMFC/MPU6050_test_YMFC.ino
+++ MPU6050_test_YMFC/MPU6050_test_YMFC.ino
@@ -9,7 +9,7 @@ byte highByte, lowByte;
 void setup(){
   Wire.begin();
   Serial.begin(9600);
-
+  set_mpu_register();
   /*Wire.beginTransmission(0x68);
   Wire.write(0x20);
   Wire.write(0x0F);
@@ -20,41 +20,17 @@ void setup(){
   Wire.write(0x10);
   Wire.endTransmission();*/
 
-  Wire.beginTransmission(0b1101000); //This is the I2C address of the MPU (b1101000/b1101001 for AC0 low/high datasheet sec. 9.2)
-  Wire.write(0x6B); //Accessing the register 6B - Power Management (Sec. 4.28)
-  Wire.write(0b00000000); //Setting SLEEP register to 0. (Required; see Note on p. 9)
-  Wire.endTransmission();  
-  
-  Wire.beginTransmission(0b1101000); //I2C address of the MPU
-  Wire.write(0x1B); //Accessing the register 1B - Gyroscope Configuration (Sec. 4.4) 
-  Wire.write(0x00000000); //Setting the gyro to full scale +/- 250deg./s 
-  Wire.endTransmission(); 
-  
-  Wire.beginTransmission(0b1101000); //I2C address of the MPU
-  Wire.write(0x1C); //Accessing the register 1C - Acccelerometer Configuration (Sec. 4.5) 
-  Wire.write(0b00000000); //Setting the accel to +/- 2g
-  Wire.endTransmission();
-
-  delay(250);
-
   Serial.print("Starting Calibration...");
   for(cal_int = 0; cal_int < 2000; cal_int ++){
     gyro_signalen();
-    
     gyro_roll_cal += gyro_roll;
     gyro_pitch_cal += gyro_pitch;
     gyro_yaw_cal += gyro_yaw;
-
-    if(cal_int%100 == 0)Serial.print(".");
-    delay(4);
-    }
-
+  }
     Serial.println("done!");
-
     gyro_roll_cal /= 2000;
     gyro_pitch_cal /= 2000;
-    gyro_yaw_cal /= 2000;
-    
+    gyro_yaw_cal /= 2000;    
   }
 
 void loop(){
@@ -87,27 +63,26 @@ void gyro_signalen(){
   if(cal_int == 2000)gyro_yaw -=gyro_yaw_cal;
   }
 
-void print_output(){
+void set_mpu_register(){
+  Wire.beginTransmission(0x68); //This is the I2C address of the MPU (b1101000/b1101001 for AC0 low/high datasheet sec. 9.2)
+  Wire.write(0x6B); //Accessing the register 6B - Power Management (Sec. 4.28)
+  Wire.write(0x00); //Setting SLEEP register to 0. (Required; see Note on p. 9)
+  Wire.endTransmission();  
   
-  Serial.print("Pitch:");
-  if(gyro_pitch>=0)Serial.print("+");
-  Serial.print(gyro_pitch/57.14286,0);
-  if(gyro_pitch/57.14286-2>0)Serial.print("NoU");
-  else if(gyro_pitch/57.14286+2<0)Serial.print("NoD");
-  else Serial.print("---");
+  Wire.beginTransmission(0x68); //I2C address of the MPU
+  Wire.write(0x1B); //Accessing the register 1B - Gyroscope Configuration (Sec. 4.4) 
+  Wire.write(0x08); //Setting the gyro to full scale +/- 500deg./s 
+  Wire.endTransmission(); 
   
-  Serial.print("Roll:");
-  if(gyro_roll>=0)Serial.print("+");
-  Serial.print(gyro_roll/57.14286,0);
-  if(gyro_roll/57.14286-2>0)Serial.print("NwU");
-  else if(gyro_roll/57.14286+2<0)Serial.print("NwD");
-  else Serial.print("---");
+  /*Wire.beginTransmission(0x68); //I2C address of the MPU
+  Wire.write(0x1C); //Accessing the register 1C - Acccelerometer Configuration (Sec. 4.5) 
+  Wire.write(0x10); //Setting the accel to +/- 8g
+  Wire.endTransmission();*/
 
-  Serial.print("Yaw:");
-  if(gyro_yaw>=0)Serial.print("+");
-  Serial.print(gyro_yaw/57.14286,0);
-  if(gyro_yaw/57.14286-2>0)Serial.print("NoR");
-  else if(gyro_yaw/57.14286+2<0)Serial.print("NoL");
-  else Serial.print("---");
-  Serial.print("\n");
+}
+
+void print_output(){
+  Serial.print("gyro_roll: ");Serial.print(gyro_roll/65.5);Serial.print("\t");
+  Serial.print("gyro_pitch: ");Serial.print(gyro_pitch/65.5);Serial.print("\t");
+  Serial.print("gyro_yaw: ");Serial.print(gyro_yaw/65.5);Serial.print("\t");
   }
