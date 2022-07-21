23d50a00e4e9c929963b499afbfb957938953bea
LUUNHATHUY53 <73783501+LUUNHATHUY53@users.noreply.github.com>
2022-07-21 22:15:20 +0700

    Update ESC_Test_DC.ino
    time 3

--- ESC_Test_DC/ESC_Test_DC.ino
+++ ESC_Test_DC/ESC_Test_DC.ino
@@ -3,16 +3,16 @@
 void setup(){
   Serial.begin(9600);
   Wire.begin();
-  TWBR = 12;
+  //TWBR = 12;
   //Arduino (Atmega) pins default to inputs, so they don't need to be explicitly declared as inputs.
   //DDRD |= B11110000;
-  analogWrite(5, 25);
+  analogWrite(5, 191);
   pinMode(6, OUTPUT);
  }
  
 void loop(){
     digitalWrite(6, HIGH);
-    delayMicroseconds(100);
+    delayMicroseconds(600);
     digitalWrite(6, LOW);
     delayMicroseconds(900);
     
